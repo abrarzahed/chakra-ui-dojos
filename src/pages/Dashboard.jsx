0605eb65208f3e9ccf134cb3390f553ef9d14a77
abrarzahed <abrarzahed6986@gmail.com>
2023-01-26 20:14:48 +0600

    coverd chakra grid system

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,36 +1,22 @@
-import { Box, Container, Heading, Text } from "@chakra-ui/react";
+import { Box, SimpleGrid } from "@chakra-ui/react";
 
 export default function Dashboard() {
-  const boxStyles = {
-    padding: "10px",
-    background: "purple.400",
-    color: "white",
-    margin: "10px",
-    textAlign: "center",
-    filter: "blur(2px)",
-    ":hover": {
-      background: "red.400",
-      filter: "blur(0px)",
-    },
-  };
   return (
-    <Container as="section" maxWidth="4xl" paddingBlock="2rem">
-      <Heading marginBlock="30px" padding="10px">
-        Chakra ui components
-      </Heading>
-      <Text marginLeft="30px">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore
-        impedit consectetur maxime sapiente
-      </Text>
-      <Text marginLeft="30px" color="blue.400" fontWeight="bold">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore
-        impedit consectetur maxime sapiente
-      </Text>
-      <Box marginBlock="30px" padding="20px" background="orange">
-        <Text color="white">This is a simple box</Text>
-      </Box>
+    <SimpleGrid spacing={10} minChildWidth="250px" p="1rem">
+      <Box bg="white" height="200px" border="1px solid"></Box>
+      <Box bg="white" height="200px" border="1px solid"></Box>
+      <Box bg="white" height="200px" border="1px solid"></Box>
+      <Box bg="white" height="200px" border="1px solid"></Box>
 
-      <Box sx={boxStyles}>Hello Ninjas</Box>
-    </Container>
+      <Box bg="white" height="200px" border="1px solid"></Box>
+      <Box bg="white" height="200px" border="1px solid"></Box>
+      <Box bg="white" height="200px" border="1px solid"></Box>
+      <Box bg="white" height="200px" border="1px solid"></Box>
+
+      <Box bg="white" height="200px" border="1px solid"></Box>
+      <Box bg="white" height="200px" border="1px solid"></Box>
+      <Box bg="white" height="200px" border="1px solid"></Box>
+      <Box bg="white" height="200px" border="1px solid"></Box>
+    </SimpleGrid>
   );
 }
