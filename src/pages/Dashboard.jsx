a88786b629beb6ddefb298015f1901fb2ecf0354
abrarzahed <abrarzahed6986@gmail.com>
2023-01-26 20:29:57 +0600

    covered chakra responsive design system

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,13 @@
-import { Box, SimpleGrid } from "@chakra-ui/react";
+import { Box, SimpleGrid, Text } from "@chakra-ui/react";
 
 export default function Dashboard() {
   return (
     <SimpleGrid spacing={10} minChildWidth="250px" p="1rem">
-      <Box bg="white" height="200px" border="1px solid"></Box>
+      <Box bg="white" height="200px" border="1px solid">
+        <Text color={{ base: "red", sm: "yellow", md: "orange", lg: "green" }}>
+          Hello world
+        </Text>
+      </Box>
       <Box bg="white" height="200px" border="1px solid"></Box>
       <Box bg="white" height="200px" border="1px solid"></Box>
       <Box bg="white" height="200px" border="1px solid"></Box>
